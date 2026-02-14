bdc420bd961100dc6e8eb64303fd23c539831d76
sammisam8888 <samuelpriyatam@gmail.com>
2026-02-15 00:39:29 +0530

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [ 
-    react(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: "/",   // ✅ keep this for custom domain
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
- 
+});
